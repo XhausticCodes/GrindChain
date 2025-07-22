63fbdf9cdd6b55b00fe84a460380174a5bd00f93
GARVIT THAKRAL <garvitthakral1001@gmail.com>
2025-07-22 16:14:52 +0530

    de-bug DB connection

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,7 @@ app.use(express.json({limit: "40KB"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
+console.log(URL);
 
 mongoose
   .connect(URL)

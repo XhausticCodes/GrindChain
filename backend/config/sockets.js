f80c7b3788baa4ee920dcfcb8968daff6c2adb53
GARVIT THAKRAL <garvitthakral1001@gmail.com>
2025-07-22 18:27:59 +0530

    backend for chat
    updated: backend Api from 5000 to 5001, did same in vite.config.js and also in .env, Major part : implemented backend for chat and task completed, created a taskSchema and updated it to user model

--- backend/config/sockets.js
+++ backend/config/sockets.js
@@ -1,4 +1,5 @@
 import { Server } from "socket.io";
+import User from "../models/User";
 
 const connectToSockets = (server) => {
   const io = new Server(server, {
@@ -12,6 +13,42 @@ const connectToSockets = (server) => {
 
   io.on("connection", (socket) => {
     console.log("A User Connected", socket.id);
+
+    socket.on("joinGroup", ({ groupID, username }) => {
+      socket.join(groupID);
+      console.log(`${username} joined group ${groupID}`);
+      socket.to(groupID).emit("userJoined", { username, groupID });
+    });
+
+    socket.on("sendMessage", ({ groupID, message, username }) => {
+      console.log(`Message from ${username} in group ${groupID}: ${message}`);
+      io.to(groupID).emit("receiveMessage", { message, username });
+    });
+
+    socket.on("taskCompleted", async ({ taskID, groupID, username }) => {
+      try {
+        const currUser = await User.findOneAndUpdate(
+          { username: username, "tasks._id": taskID },
+          { $set: { "tasks.$.completed": true } },
+          { new: true }
+        );
+
+        if (!currUser) {
+          return socket.emit("task-completion-error", {
+            error: "User or task not found",
+          });
+        }
+      } catch (e) {
+        console.error(err);
+        socket.emit("task-update-error", {
+          error: "Server error while updating task",
+        });
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("A User Disconnected", socket.id);
+    });
   });
 };
 
